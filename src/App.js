5c9bcc332bdba443eb39f30fb161decf2ff21217
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-04-22 21:49:25 +0530

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,53 +1,30 @@
 import React, { useState, useEffect } from "react";
 
-function App() {
-  //usestate is a hook :fxn that lets you hook into react feature ,lets you create local state for fxn comp
-  //setstate will need initial state as argument and will pass an array
-  //[0] initial state ,[1] function to change state
+// custom hook
+// using react hook inside a fxn whose name starts with use is custom hook
+function useFormInputs(initialValue) {
+  const [value, setValue] = useState(initialValue);
 
-  const [userId, setUserId] = useState("1");
-  const [data, setData] = useState([]);
-
-  // use effect will be called on first render and after every update ,setDate(),setUserId  rerenders the comp;
-  // with empty array only on first render ,will act as didmount
-  // with dependencies on first render and when dependencies changes   ,will act like didupdate
-  // need to pass callback to useEffect
-  useEffect(() => {
-    const url = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, [userId]);
-
-  useEffect(() => {
-    document.addEventListener("mouseover", onMouseMove); // add event listeneer
-
-    // remove event listener when component is destroyed to avoid memory leak ,
-    // return function part is totally optional
-    // component will unmount alternative
-    return () => {
-      document.removeEventListener("mouseover", onMouseMove);
-    };
-  });
-  function onMouseMove(event) {
-    console.log(event.clientX);
+  function handleChange(e) {
+    setValue(e.target.value);
   }
+  return {
+    value,
+    onChange: handleChange,
+  };
+}
+
+function App() {
+  const email = useFormInputs("a");
+
+  const name = useFormInputs("Rk");
+
   return (
     <div>
-      <h1>App</h1>
-      <button
-        onClick={() => {
-          setUserId("2");
-        }}
-      >
-        Change userid to 2
-      </button>
-      {data.map((user) => (
-        <p>{user.title}</p>
-      ))}
+      <input value={email.value} onChange={email.onChange} />
+      <input value={name.value} onChange={name.onChange} />
+      <p>Email: {email.value}</p>
+      <p>Name: {name.value}</p>
     </div>
   );
 }

0ad59fe20300b9d7059dc14d37413a96c28aa672
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-04-22 16:52:48 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App">Hello</div>;
+  //usestate is a hook :fxn that lets you hook into react feature ,lets you create local state for fxn comp
+  //setstate will need initial state as argument and will pass an array
+  //[0] initial state ,[1] function to change state
+  const EmailState = useState("");
+  const email = EmailState[0];
+  const setEmail = EmailState[1];
+
+  const [name, setname] = useState("");
+
+  function handleChange(e) {
+    setEmail(e.target.value);
+  }
+  function handleNameChange(e) {
+    setname(e.target.value);
+  }
+
+  return (
+    <div>
+      <input value={email} onChange={handleChange} />
+      <input value={name} onChange={handleNameChange} />
+      <p>Email: {email}</p>
+      <p>Name: {name}</p>
+    </div>
+  );
 }
 
 export default App;
